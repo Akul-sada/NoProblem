b5f01a4a19f42638646def55f5c43297271977db
Akul-sada <sadananda.sherigara@gmail.com>
2024-11-16 07:42:40 +0530

    write the affermation add styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [problemName,setProblemName] = useState("");
+  const [personName,setPersonName] = useState("");
+  
+  const submitHandler =()=>{
+
+    setPersonName("");
+    setProblemName("");
+  }
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <form className='form-gap'>
+          <label>Name of the Person
+          <input style={{marginLeft:'32px'}} type='text' value={personName} onChange={(e)=>setPersonName(e.target.value)}/>
+          </label>
+          <label>Name of the problem
+          <input style={{marginLeft:'20px'}} type='text' value={problemName} onChange={(e)=>setProblemName(e.target.value)}/>
+          </label>          
+          <input style={{background:'green',border:'white',color:'#fff'}} type='submit' className='btn' onSubmit={submitHandler}/>
+
+        </form>
+        <p style={{color:'yellow'}}>
+          {personName} donot have {problemName} problem.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
