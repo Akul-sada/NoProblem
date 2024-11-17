034948ead4ee1c6245be4f9f760f0f47cfee81a9
Akul-sada <sadananda.sherigara@gmail.com>
2024-11-17 14:41:52 +0530

    make webpage responsive

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { useState} from 'react';
 import Modal from 'react-modal';
-import sound from './static/sound1.mp3'
+import sound from './static/sound1.mp3';
+
 
 
 function play(){
@@ -40,34 +41,37 @@ function App() {
       <button className='hidden' onClick={()=>play()}>Sound</button>
               {/* modal code starts */}
       <div>
-      <button onClick={openModal} className='btn' style={{color:'white',border:'white',cursor:'pointer',background:'green'}}>Click here to make Payment</button>
+      <button onClick={openModal} className='green-btn'>Click here to make Payment</button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={{background:'red'}}
-        contentLabel="Example Modal"
       >
-        <img alt="Payment QR Code" style={{maxWidth:'300px'}} src='./image.jpg'/>
-      <button onClick={closeModal} style={{position:'absolute',left:'344px',cursor:'pointer'}}>close</button>
+      <div className='payment-modal'>
+
+
+        <button className="close-payment btn" onClick={closeModal}>close</button>
+
+        <img alt="Payment QR Code" style={{maxWidth:'100%'}} src='./image.jpg'/>
+      </div>
       </Modal>
     </div>
     {/* Modal code ends */}   
-        <h1>Fill in the form and your problem will be solved.</h1>  
+        <h1 className='heading'>Fill in the form and your problem will be solved.</h1>  
         
         <img src={logo} className="App-logo" alt="logo" />
         <form className='form-gap'>
           <label>Name of the Person 👉
-          <input required placeholder='eg:- Ranveer Singh' style={{marginLeft:'32px'}} type='text' value={personName} onChange={(e)=>setPersonName(e.target.value)}/>
+          <input required placeholder='example:- Ranveer Singh' style={{marginLeft:'3.2rem'}} type='text' value={personName} onChange={(e)=>setPersonName(e.target.value)}/>
           </label>
           <label>Name of the problem 👉
-          <input required placeholder='eg:- Heart or liver' style={{marginLeft:'20px'}} type='text' value={problemName} onChange={(e)=>setProblemName(e.target.value)}/>
+          <input required placeholder='example:- Heart or liver' style={{marginLeft:'2rem'}} type='text' value={problemName} onChange={(e)=>setProblemName(e.target.value)}/>
           </label>          
-          <input style={{background:'green',border:'white',color:'#fff'}} type='submit' className='btn' onSubmit={submitHandler}/>
+          <input className="green-btn center" type='submit' onSubmit={submitHandler}/>
         </form>
-        <p style={{color:'yellow',fontSize:'60px',margin:0}}>
+        <p style={{color:'yellow',fontSize:'3rem',margin:0}}>
           {personName} donot have {problemName} problem.
         </p>
-
       </header>
     </div>
   );
